ae5a88ed6f46927e838f46f1cd882d8e7ad47c71
insanusconcursos-edtec <insanusconcursos@gmail.com>
2026-05-10 20:56:12 -0500

    ATUALIZAÇÃO: correção da rota de calendário.

--- pages/student/StudentCalendar.tsx
+++ pages/student/StudentCalendar.tsx
@@ -1,4 +1,5 @@
 
+import React, { useState, useEffect } from 'react';
 import { StudyCalendar } from '../../components/student/calendar/StudyCalendar';
 import { useAuth } from '../../contexts/AuthContext';
 import { PlanHeroBanner } from '../../components/student/PlanHeroBanner';
